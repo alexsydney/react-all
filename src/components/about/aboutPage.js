09097326b2acb041666afe38313226270a093474
Luke Schoen <ltfschoen@gmail.com>
2016-11-21 15:46:44 +1100

    Applied React Router Redirect Component and Static Methods for Transition From/To

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,21 @@
 var React = require('react');
 
 var About = React.createClass({
+    // Static Methods
+    statics: {
+        willTransitionTo: function (transition, params, query, callback) {
+            if(!confirm('Are you you over 18?')) {
+                transition.abort();
+            } else {
+                callback();
+            }
+        },
+        willTransitionFrom: function (transition, component) {
+            if(!confirm('Did you read about us?')) {
+                transition.abort();
+            }
+        }
+    },
     render: function () {
         return (
             <div>
--- src/routes.js
+++ src/routes.js
@@ -7,22 +7,27 @@ var React = require('react');
 // Specific router functions with reference to Router
 var Router = require('react-router');
 
+// Reference to Route Component that defines routes
+var Route = Router.Route;
+
 // Reference to DefaultRoute to declare route to load when page loads at root URL
 var DefaultRoute = Router.DefaultRoute;
 
 // Reference to NotFoundRoute Component that comes with React Router
 var NotFoundRoute = Router.NotFoundRoute;
 
-// Reference to Route Component that defines routes
-var Route = Router.Route;
+var Redirect = Router.Redirect;
 
-// Define routes and Components that handle respective routes when called
+// Define routing rules and Components that handle respective routes when called
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
         <DefaultRoute handler={require('./components/homePage')} />
         <Route name="skills" handler={require('./components/skills/skillPage')} />
         <Route name="about" handler={require('./components/about/aboutPage')} />
         <NotFoundRoute handler={require('./components/notFoundPage')} />
+        {/* Redirect Subdirectories (Bad References) */}
+        <Redirect from="skills/*" to="skills" />
+        <Redirect from="about/*" to="about" />
     </Route>
 );
 
