6b077a788fa27ffb07e3659955e23752db965e80
Luke Schoen <ltfschoen@gmail.com>
2016-11-22 19:49:05 +1100

    Added Skill Flux Store core functionality by extending EventEmitter.prototype

---
+++ src/flux/stores/storeSkill.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// Import core libraries to create new Store
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../../constants/flux/typesSkill.js');
+
+// Node.js EventEmitter broadcasts events from Stores to notify React Components
+var EventEmitter = require('events').EventEmitter;
+
+/**
+ *  Extend the Flux Store to have Event Emitter capabilities using object-assign library
+ *  that joins two objects and their properties together (i.e. SkillStore and EventEmitter prototype)
+ *  https://www.npmjs.com/package/object-assign
+ */
+var assign = require('object-assign');
+
+/**
+ *  Extend the Flux Store using object-assign library by
+ *  passing empty base object, and extend utilising:
+ *      - EventEmitter.prototype parameter
+ *      - block that defines the Store parameter
+ *  Essentially this defines EventEmitter.prototype as the base class
+ */
+var SkillStore = assign({}, EventEmitter.prototype, {
+
+    /**
+     *  Define Flux Store providing three core helper functions for React Components to interact with it
+     *  utilising the EventEmitter.prototype interface and functionality
+     *      - addChangeListener (accepts a callback parameter)
+     *      - removeChangeListener
+     *      - emitChange
+     */
+    addChangeListener: function(callback) {
+
+        // Call the callback whenever change occurs in this Store
+        this.on('change', callback);
+    },
+
+    removeChangeListener: function (callback) {
+        this.removeListener('change', callback);
+    },
+    
+    emitChange: function () {
+        this.emitChange();
+    }
+
+});
